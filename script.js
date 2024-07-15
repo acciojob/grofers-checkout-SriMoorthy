0b1fa31d702fcdb094ad87353cde4e6e9f365c49
SriMoorthy <srimoorthy27@gmail.com>
2024-07-15 14:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,3 +23,36 @@ const getSum = () => {
 
   getSumBtn.addEventListener("click", getSum);
 
+
+() => {
+  cy.visit("http://localhost:3000/");
+  
+  // Ensure the price elements are editable and set their values
+  cy.get(".price").each(($el, index) => {
+    cy.wrap($el).invoke("attr", "contenteditable", "true");
+    switch (index) {
+      case 0:
+        cy.wrap($el).clear().type("20");
+        break;
+      case 1:
+        cy.wrap($el).clear().type("33");
+        break;
+      case 2:
+        cy.wrap($el).clear().type("0");
+        break;
+      case 3:
+        cy.wrap($el).clear().type("70");
+        break;
+      case 4:
+        cy.wrap($el).clear().type("210");
+        break;
+    }
+  });
+
+  // Click the button to trigger the total price calculation
+  cy.get("button").click();
+
+  // Wait for the result to be calculated and displayed
+  cy.get("#ans", { timeout: 10000 }).should("contain", "333");
+}
+
